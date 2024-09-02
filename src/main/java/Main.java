4d11fcaa234786daecd379f112254dbbd24ce3c5
pedroferens <169068429+pedroferens@users.noreply.github.com>
2024-09-02 12:37:55 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+   //System.out.println("Entre com um numero inteiro: ");
+   //int inteiro = sc.nextInt();
+    //System.out.println("O numero digitado foi: ");
+    //System.out.println(inteiro);
+
+    //System.out.println("Entre com um numero racional: ");
+    //double racional = sc.nextDouble();
+    //System.out.println("O numero digitado foi: ");
+    //System.out.println(racional);
+
+    System.out.println("Entre com o nome de seu futuro filho(a): ");
+    String racional = sc.nextDouble();
+    System.out.println("O numero digitado foi: ");
+    System.out.println(racional);
+    
+    sc.close();
+    
+  }
+
+  
+  
+  
+
+}
